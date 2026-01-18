f2fb3702da3c62026fe4494727d6c4670cbc3722
Arslan Shafiq <charslanshafiq4@gmail.com>
2026-01-18 17:07:21 +0500

    Add Render port configuration

--- My Portfolio/Program.cs
+++ My Portfolio/Program.cs
@@ -1,6 +1,9 @@
 using My_Portfolio;
 
 var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
